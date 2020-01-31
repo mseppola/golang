6fc324a75caed2fccadfacedbeab71462be9ddd4
Miles Seppola <mseppola@mseppola-centos.sjrb.ad>
2020-01-31 09:05:24 -0700

    Logging Commit

--- logging/logging.go
+++ logging/logging.go
@@ -1,9 +1,20 @@
 package main
 
 import (
-  "github.com/golang/glog"
+	"log"
+	"os"
 )
 
-func main () {
-  glog.Info ( "glog Info Message" )
+func main() {
+
+	file, err := os.OpenFile("/home/mseppola/go/log/logging", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer file.Close()
+
+	log.SetOutput(file)
+	log.Println("Logging to a file in Go!")
 }
